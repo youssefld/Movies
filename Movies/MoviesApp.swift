920dcc793cfca6e014cd18fea2ae30afc8999154
Youssef Lakouifat <youssef.lakouifat@wuolah.com>
2023-01-01 15:19:56 +0100

    First commit

---
+++ Movies/MoviesApp.swift
@@ -0,0 +1,17 @@
+//
+//  MoviesApp.swift
+//  Movies
+//
+//  Created by Youssef Lakouifat on 1/1/23.
+//
+
+import SwiftUI
+
+@main
+struct MoviesApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
